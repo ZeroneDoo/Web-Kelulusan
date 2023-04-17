47c25f6c8315c411aeebf6cf614e97ecc62c9518
ZeroneDoo <rizqipambudi6@gmail.com>
2023-04-17 13:38:55 +0700

    Upload File

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,17 @@
 <?php
 
+use App\Imports\MatpelImport;
+use App\Imports\UsersImport;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Maatwebsite\Excel\Facades\Excel;
 
 Route::get('/', function () {
-    // return view('welcome');
-    dd(User::with(["kelas"])->get());
+    return view('test');
+});
+
+Route::post('/', function(Request $request){
+    $request->file->storeAs('public/files', "asdad".$request->file->extension);
+    return redirect('/');
 });
