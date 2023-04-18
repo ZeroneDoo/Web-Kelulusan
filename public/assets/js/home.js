bbdb0cdf0aee1cf4a2a9a8968c484c467dd4d05f
sheylaulya <sheylaulyaa@gmail.com>
2023-04-19 00:08:01 +0700

    Add Home View Features

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,23 +1,23 @@
-var countDownDate = new Date('May 05, 2023 00:00:00').getTime();
-var x = setInterval(
-    function () {
-        var now = new Date().getTime();
-        var distance = countDownDate - now;
+var countDownDate = new Date('April 19, 2024 00:06:00').getTime();
+        var x = setInterval(
+            function () {
+                var now = new Date().getTime();
+                var distance = countDownDate - now;
 
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        let day = document.getElementById('days').innerHTML = days;
-        let hour = document.getElementById('hours').innerHTML = hours;
-        let minute = document.getElementById('minutes').innerHTML = minutes;
-        let seccond = document.getElementById('seconds').innerHTML = seconds;
+                var day = document.getElementById('days').innerHTML = days;
+                let hour = document.getElementById('hours').innerHTML = hours;
+                let minute = document.getElementById('minutes').innerHTML = minutes;
+                let seccond = document.getElementById('seconds').innerHTML = seconds;
 
-        // if(day == "00"){
-        //     console.log('dibuka')
-        // }
-    }, 1000
-
-
-)
+                if (seconds <= "00") {
+                      clearInterval(x);
+                    document.getElementById("left-count").style.display = "none";
+                    document.getElementById("left").style.display = "block";
+                }
+            }, 1000
+        )
