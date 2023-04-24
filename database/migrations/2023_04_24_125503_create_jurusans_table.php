cb1acb647914421c220610e9e2bd3fa5c02ac620
ZeroneDoo <rizqipambudi6@gmail.com>
2023-04-24 20:21:08 +0700

    Add Generate PDF

--- database/migrations/2023_04_24_125503_create_jurusans_table.php
+++ database/migrations/2023_04_24_125503_create_jurusans_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('matpel_kejuruans', function (Blueprint $table) {
+        Schema::create('jurusans', function (Blueprint $table) {
             $table->id();
+            $table->string('jurusan');
             $table->timestamps();
         });
     }
@@ -26,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('matpel_kejuruans');
+        Schema::dropIfExists('jurusans');
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,18 @@ use App\Imports\UsersImport;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Maatwebsite\Excel\Facades\Excel;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 Route::get('/', function () {
-    return view('home');
+    return view('tes');
 });
 
 Route::post('/', function(Request $request){
     Excel::import(new UsersImport, $request->file('file'));
     return redirect('/');
 });
+
+Route::get('/generatepdf', function(){
+    $pdf = Pdf::loadView('page.testing', ['data' => 1]);
+    return $pdf->download('testing.pdf');
+})->name('generatepdf');
