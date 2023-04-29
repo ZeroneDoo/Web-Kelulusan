989e678074fedd1c794bcc30cac08736b6c69d9d
sheylaulya <sheylaulyaa@gmail.com>
2023-04-29 21:02:19 +0700

    Revisi Export Nilai Page

--- resources/views/dashboard/exportTemplate.blade.php
+++ resources/views/dashboard/exportTemplate.blade.php
@@ -9,15 +9,16 @@
 <body>
     <div class="container" >
         
-    {{-- <img src="assets/img/kopSurat.png" style="  margin-top:-50px;" alt=""> --}}
+    <img src="assets/img/kopSurat.png" style="height:230px;  margin-top:-50px; " alt="">
 
-        <h5 style="text-align: center;"><u>SURAT KETERANGAN LULUS</u></h5> 
-        <div class="content" style="padding-left: 50px; padding-right: 50px">
-            <p style="font-size: 14px">Kepala Sekolah SMK Taruna Bhakti Depok selaku Ketua Penyelenggaran Ujian Sekolah Tahun Pelajaran 2022/2023 berdasarkan : </p>
+        <h4 style="text-align: center; margin-top:-2px;"><u>SURAT KETERANGAN LULUS</u></h4> 
+        <p style="text-align: center; font-size:12px; margin-top:-18px;">Nomor :  49/421.5-SMK.TB/KS/V/2023</p>
+        <div class="content" style="padding-left: 50px; padding-right: 50px;">
+            <p style="font-size: 14px">Kepala SMK Taruna Bhakti selaku Ketua Penyelenggara Ujian Sekolah Tahun Pelajaran 2022/2023 berdasarkan : </p>
             <ol>
                 <li style="font-size: 14px">Ketuntasan dari seluruh program pembelajaran pada kurikulum 2013;</li>
-                <li style="font-size: 14px">Kriteria kelulusan dari satuan pendidikan sesuai dengan peraturan perundang - undangan;</li>
-                <li style="font-size: 14px">Rapat Pleno Dewan Guru tentang Kelulusan pada tanggal 04 Mei 2023</li>
+                <li style="font-size: 14px">Kriteria kelulusan dari satuan pendidikan sesuai dengan peraturan perundang-undangan ;</li>
+                <li style="font-size: 14px">Rapat Pleno Dewan Guru tentang Kelulusan pada tanggal 04 Mei 2023.</li>
             </ol>
             <p style="font-size: 14px">Menerangkan bahwa :</p>
             <table>
@@ -59,124 +60,113 @@
                 </tr>
               
             </table>
-            <p style="font-size: 14px">dengan nilai sebagai berikut :</p>
+            <p style="font-size: 14px;">dengan nilai sebagai berikut :</p>
             <table border="1" style="border-collapse: collapse;">
                 <tr>
-                  <th style="border: 1px solid black; width: 50px; font-size: 12px">No.</th>
-                  <th style="border: 1px solid black; width: 450px; font-size: 12px">Mata Pelajaran</th>
-                  <th style="border: 1px solid black; width: 70px;font-size: 12px">Nilai</th>
+                  <th style="border: 1px solid black; width: 50px; font-size: 14px">No.</th>
+                  <th style="border: 1px solid black; width: 450px; font-size: 14px">Mata Pelajaran</th>
+                  <th style="border: 1px solid black; width: 70px;font-size: 14px">Nilai</th>
                 </tr>
                 <tr>
-                    <td colspan="2" style="padding-left: 13px; font-size: 12px">Muatan Nasional</td>
+                    <td colspan="2" style="padding-left: 14px; font-size: 11px"><b>Muatan Nasional</b></td>
                     <td></td>
                 </tr>
                 <!-- Muatan Nasional -->
                 <tr>
-                    <td style="text-align: center; font-size: 12px"> 1.</td>
-                    <td style="padding-left: 10px; font-size: 12px"> Pendidikan Agama dan Budi Pekerti</td>
-                    <td style="text-align: center;font-size: 12px">90</td>
+                    <td style="text-align: center; font-size: 11px"> 1.</td>
+                    <td style="padding-left: 10px; font-size: 11px"> Pendidikan Agama dan Budi Pekerti</td>
+                    <td style="text-align: center;font-size: 11px">90</td>
                 </tr>
                 <tr>
-                    <td style="text-align: center;font-size: 12px"> 2.</td>
-                    <td style="padding-left: 10px;font-size: 12px"> Pendidikan Pancasila dan Kewarganegaraan</td>
-                    <td style="text-align: center;font-size: 12px">90</td>
+                    <td style="text-align: center;font-size: 11px"> 2.</td>
+                    <td style="padding-left: 10px;font-size: 11px"> Pendidikan Pancasila dan Kewarganegaraan</td>
+                    <td style="text-align: center;font-size: 11px">90</td>
                 </tr>
                 <tr>
-                    <td style="text-align: center; font-size:12px;"> 3.</td>
-                    <td style="padding-left: 10px; font-size:12px;"> Bahasa Indonesia</td>
-                    <td style="text-align: center; font-size:12px;">90</td>
+                    <td style="text-align: center; font-size:11px;"> 3.</td>
+                    <td style="padding-left: 10px; font-size:11px;"> Bahasa Indonesia</td>
+                    <td style="text-align: center; font-size:11px;">90</td>
                 </tr>
                 <tr>
-                    <td style="text-align: center; font-size:12px;"> 4.</td>
-                    <td style="padding-left: 10px; font-size:12px;"> Matematika</td>
-                    <td style="text-align: center; font-size:12px;">90</td>
+                    <td style="text-align: center; font-size:11px;"> 4.</td>
+                    <td style="padding-left: 10px; font-size:11px;"> Matematika</td>
+                    <td style="text-align: center; font-size:11px;">90</td>
                 </tr>
                 <tr>
-                    <td style="text-align: center; font-size:12px;"> 5.</td>
-                    <td style="padding-left: 10px; font-size:12px;"> Sejarah Indonesia</td>
-                    <td style="text-align: center; font-size:12px;">90</td>
+                    <td style="text-align: center; font-size:11px;"> 5.</td>
+                    <td style="padding-left: 10px; font-size:11px;"> Sejarah Indonesia</td>
+                    <td style="text-align: center; font-size:11px;">90</td>
                 </tr>
                 <tr>
-                    <td style="text-align: center; font-size:12px;"> 6.</td>
-                    <td style="padding-left: 10px; font-size:12px;"> Bahasa Inggris dan Bahasa Asing Lainnya</td>
-                    <td style="text-align: center; font-size:12px;">90</td>
+                    <td style="text-align: center; font-size:11px;"> 6.</td>
+                    <td style="padding-left: 10px; font-size:11px;"> Bahasa Inggris</td>
+                    <td style="text-align: center; font-size:11px;">90</td>
                 </tr>
 
                 <!-- Muatan Kewilayahan -->
                 <tr>
-                    <td colspan="2" style="padding-left: 13px; font-size:12px;">Muatan Kewilayahan</td>
+                    <td colspan="2" style="padding-left: 14px; font-size:11px;"><b>Muatan Kewilayahan</b></td>
                     <td></td>
                 </tr>
                 <tr>
-                    <td style="text-align: center; font-size:12px;"> 1.</td>
-                    <td style="padding-left: 10px; font-size:12px;"> Seni Budaya</td>
-                    <td style="text-align: center; font-size:12px;">90</td>
+                    <td style="text-align: center; font-size:11px;"> 1.</td>
+                    <td style="padding-left: 10px; font-size:11px;"> Seni Budaya</td>
+                    <td style="text-align: center; font-size:11px;">90</td>
                 </tr>
                 <tr>
-                    <td style="text-align: center; font-size:12px;"> 2.</td>
-                    <td style="padding-left: 10px; font-size:12px;"> Pendidikan Jasmani,Olahraga dan Kesehatan</td>
-                    <td style="text-align: center; font-size:12px;">90</td>
+                    <td style="text-align: center; font-size:11px;"> 2.</td>
+                    <td style="padding-left: 10px; font-size:11px;"> Pendidikan Jasmani,Olahraga dan Kesehatan</td>
+                    <td style="text-align: center; font-size:11px;">90</td>
                 </tr>
                 <tr>
-                    <td style="text-align: center;" rowspan="4; font-size:12px;"> 3.</td>
-                    <td style="padding-left: 10px; font-size:12px;"> Muatan Lokal</td>
-                    <td style="text-align: center; font-size:12px;">90</td>
+                    <td style="text-align: center; font-size:11px;"> 3.</td>
+                    <td style="padding-left: 10px; font-size:11px;"> Bahasa Sunda</td>
+                    <td style="text-align: center; font-size:11px;">90</td>
                 </tr>
-                <tr>
-                    <td style="padding-left: 10px; font-size:12px;"> </td>
-                    <td style="text-align: center; font-size:12px;">90</td>
-                </tr>
-                <tr>
-                    <td style="padding-left: 10px; font-size:12px;"> </td>
-                    <td style="text-align: center; font-size:12px;">90</td>
-                </tr>
-                <tr>
-                    <td style="padding-left: 10px; font-size:12px;"> </td>
-                    <td style="text-align: center; font-size:12px;">90</td>
-                </tr>
+               
                 
                 <!-- Muatan Peminatan Kejuruan -->
                 <tr>
-                    <td colspan="2" style="padding-left: 13px; font-size:12px;">Muatan Peminatan Kejuruan</td>
+                    <td colspan="2" style="padding-left: 14px; font-size:11px;"><b>Muatan Peminatan Kejuruan</b></td>
                     <td></td>
                 </tr>
                 <tr>
-                    <td style="text-align: center; font-size:12px;"> 1.</td>
-                    <td style="padding-left: 10px; font-size:12px;"> Simulasi dan Komunikasi Digital</td>
-                    <td style="text-align: center; font-size:12px;">90</td>
+                    <td style="text-align: center; font-size:11px;"> 1.</td>
+                    <td style="padding-left: 10px; font-size:11px;"> Simulasi dan Komunikasi Digital</td>
+                    <td style="text-align: center; font-size:11px;">90</td>
                 </tr>
                 <tr>
-                    <td style="text-align: center; font-size:12px;"> 2.</td>
-                    <td style="padding-left: 10px; font-size:12px;"> Fisika</td>
-                    <td style="text-align: center; font-size:12px;">90</td>
+                    <td style="text-align: center; font-size:11px;"> 2.</td>
+                    <td style="padding-left: 10px; font-size:11px;"> Fisika / Tata Artistik</td>
+                    <td style="text-align: center; font-size:11px;">90</td>
                 </tr>
                 <tr>
-                    <td style="text-align: center; font-size:12px;"> 3.</td>
-                    <td style="padding-left: 10px; font-size:12px;"> Kimia</td>
-                    <td style="text-align: center; font-size:12px;">90</td>
+                    <td style="text-align: center; font-size:11px;"> 3.</td>
+                    <td style="padding-left: 10px; font-size:11px;"> Kimia / Dasar-dasar Kreativitas 	</td>
+                    <td style="text-align: center; font-size:11px;">90</td>
                 </tr>
                 <tr>
-                    <td style="text-align: center; font-size:12px;"> 4.</td>
-                    <td style="padding-left: 10px; font-size:12px;"> Dasar Program Keahlian</td>
-                    <td style="text-align: center; font-size:12px;">90</td>
+                    <td style="text-align: center; font-size:11px;"> 4.</td>
+                    <td style="padding-left: 10px; font-size:11px;"> Dasar Program Keahlian</td>
+                    <td style="text-align: center; font-size:11px;">90</td>
                 </tr>
                 <tr>
-                    <td style="text-align: center; font-size:12px;"> 5.</td>
-                    <td style="padding-left: 10px; font-size:12px;"> Kompetensi Keahlian</td>
-                    <td style="text-align: center; font-size:12px;">90</td>
+                    <td style="text-align: center; font-size:11px;"> 5.</td>
+                    <td style="padding-left: 10px; font-size:11px;"> Kompetensi Keahlian</td>
+                    <td style="text-align: center; font-size:11px;">90</td>
                 </tr>
 
                 <tr>
-                    <td colspan="2" style="text-align: center; font-size:12px;"><b>Rata Rata</b></td>
+                    <td colspan="2" style="text-align: center; font-size:11px;"><b>Rata Rata</b></td>
                     <td></td>
                 </tr>
             </table>
-            <div class="footer" style="margin-left: 420px;">
-                <p style="font-size: 14px">Depok, 5 Mei 2023</p>
-                <p style="font-size: 14px">Kepala Sekolah,</p>
-                <div class="" style="width: 100%; height: 40px; font-size:14px;"></div>
-                <p style="font-size: 14px">Nursidik, ST</p>
-                <p style="font-size: 14px">NIP 19790911200411201</p>
+            <div class="footer" style="margin-left: 350px;">
+                <p style="font-size: 14px">Depok, 05 Mei 2023</p>
+                <p style="font-size: 14px"><b>Kepala Sekolah SMK Taruna Bhakti,</b></p>
+                <div class="" style="width: 100%; height: 20px; font-size:14px;"></div>
+                <p style="font-size: 14px"><b><u>Nursidik, ST</u></b></p>
+                {{-- <p style="font-size: 14px">NIP 19790911200411201</p> --}}
             </div>
          
         </div>  
