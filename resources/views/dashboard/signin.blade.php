2b90a14441ddf1d3bc59774a8cc6c6f3992482ae
sheylaulya <sheylaulyaa@gmail.com>
2023-05-03 19:42:57 +0700

    Revisi

--- resources/views/dashboard/signin.blade.php
+++ resources/views/dashboard/signin.blade.php
@@ -45,13 +45,7 @@
                                         </div>
                                     </form>
                                 </div>
-                                <div class="card-footer text-center pt-0 px-lg-2 px-1">
-                                    <p class="mb-4 text-sm mx-auto">
-                                        Don't have an account?
-                                        <a href="/signup" class="text-info text-gradient font-weight-bold">Sign
-                                            up</a>
-                                    </p>
-                                </div>
+                               
                             </div>
                         </div>
                         <div class="col-md-6">
