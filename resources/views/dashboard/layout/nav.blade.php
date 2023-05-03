4f0f6fc3f576a7c61fef6f5f76ceed92dc3e513e
sheylaulya <sheylaulyaa@gmail.com>
2023-05-03 13:11:06 +0700

    Revisi Export PDF & Add Logout Button

--- resources/views/dashboard/layout/nav.blade.php
+++ resources/views/dashboard/layout/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur"
     navbar-scroll="true">
-    <div class="container-fluid py-1 px-3">
+    <div class="container-fluid py-1 px-3  d-flex align-items-end">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
                 <li class="breadcrumb-item text-sm "><a class="opacity-5 text-dark text-decoration-none" href="javascript:;">Halaman</a></li>
@@ -8,6 +8,12 @@
             </ol>
             <h6 class="font-weight-bolder mb-0">@yield('titles')</h6>
         </nav>
+        
+        <button type="button" class="btn btn-danger text-sm px-4 text-capitalize" style="position: absolute; top:30px; right:0px;">
+            <b>Keluar</b>
+        </button>
     </div>
+
+    
 </nav>
 <!-- End Navbar -->
